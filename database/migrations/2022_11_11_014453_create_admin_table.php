f5d6548e916befdd7c74650b5b86923d4e0b8b48
onynovianti <95726641+onynovianti@users.noreply.github.com>
2022-11-17 11:46:05 +0700

    Add files via upload

---
+++ database/migrations/2022_11_11_014453_create_admin_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->string('namaLengkap');
+            $table->string('username')->unique();
+            $table->string('password');
+            $table->string('alamat');
+            $table->string('noHp');
+            $table->integer('jenisKelamin');
+            $table->string('tempatLahir');
+            $table->date('tanggalLahir');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+};
