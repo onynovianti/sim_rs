7a82bc9af9d4c5816281b29023816874dc5fdf3a
onynovianti <onynovianti26@gmail.com>
2022-11-23 18:24:14 +0700

    sync

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -17,7 +17,7 @@ class AdminController extends Controller
     public function index(){
        return view('pages.admin',[
             'item' => DB::table('admins')->paginate(10),
-       ]); 
+       ]);  
     }
 
     // Tampilan Create Admin
