edbeaea6cf2b2e513c2f7af4b96971f4cb3344a3
sunuraihan <sunuraihan29@gmail.com>
2022-12-27 19:56:46 +0700

    commit migration diagnosis

---
+++ database/migrations/2022_12_27_125442_create_diagnosis_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('diagnosis', function (Blueprint $table) {
+            $table->id();
+            $table->string('namaLengkap');
+            $table->integer('harga');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('diagnosis');
+    }
+};
